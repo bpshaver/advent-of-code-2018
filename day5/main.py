ea96465454fafa9e6ba5e493bcd3ee71a230b4c7
Ben Shaver <benpshaver@gmail.com>
2018-12-05 09:13:04 -0500

    Recursion is not the way is not the way not the way

--- day5/main.py
+++ day5/main.py
@@ -1,17 +1,10 @@
 from string import ascii_lowercase
 from timeit import timeit
 
-
-with open('input.txt', 'r') as f:
-    polymer = f.read().strip()
-
-assert len(set(polymer)) == 52
-assert len(set(polymer.lower())) == 26
-
 sample = 'dabAcCaCBAcCcaDA'
 
 # Part 1
-def old_react_polymer(polymer=sample):
+def react_polymer(polymer=sample):
     string_len_pre = len(polymer)
     string_len_post = 0
     while string_len_pre > string_len_post:
@@ -23,38 +16,63 @@ def old_react_polymer(polymer=sample):
         string_len_post = len(polymer)
     return polymer
 
-assert len(old_react_polymer(sample)) == 10
+assert len(react_polymer(sample)) == 10
 
 # RECURSIVE SOLUTION
-# sample = 'dabAcCaCBAcCcaDA'
-# def react_polymer(polymer=sample, verbose=False):
-#     for i, pair in enumerate(zip(polymer[:-1], polymer[1:])):
-#         letter1, letter2 = pair
-#         if letter1 != letter2 and letter1.lower() == letter2.lower():
-#             polymer = polymer[:i] + polymer[i+2:]
-#             if verbose:
-#                 print(f"Found matching pair {pair} at position {i}")
-#                 print(f"Recursing with string {polymer[:i]}|{letter1}{letter2}|{polymer[i+2:]} ==> {polymer}")
-            
-#             polymer = react_polymer(polymer)
-#     return polymer
+def left_bisect(string, i):
+    if i >= len(string):
+        return string
+    else:
+        return string[:i]
 
-# print(react_polymer(sample, verbose=True))
+def right_bisect(string, j):
+    return string[j+1:]
 
-# assert len(react_polymer(sample)) == 10
-# old_time = timeit(old_react_polymer, setup='pass', number=100)
-# recursive_time = timeit(react_polymer, setup='pass', number=100)
-# assert recursive_time < old_time
+def recursive_react_polymer(polymer=sample, verbose=False):
+    """
+    This way hits maximum recursion depth.
+    """
+    for i, j in zip(range(len(polymer)-1), range(1,len(polymer))):
+        if polymer[i] != polymer[j] and polymer[i].lower() == polymer[j].lower():
+            left_bisection  = left_bisect(polymer, i)
+            right_bisection = right_bisect(polymer, j)
+            if verbose:
+                print(f"Detected matching pair {polymer[i],polymer[j]}")
+                print(f"In string {polymer}:")
+                print(f"--{left_bisection}|{polymer[i]}{polymer[j]}|{right_bisection}")
+                print('\n')
+            recurse_left = recursive_react_polymer(left_bisection, verbose=verbose)
+            recurse_right = recursive_react_polymer(right_bisection, verbose=verbose)
+            polymer = recursive_react_polymer(recurse_left + recurse_right, verbose=verbose)
+            break
+        elif j == len(polymer):
+            return polymer
+    return polymer
+    
+assert len(recursive_react_polymer(sample)) == 10
 
 # Part 2
 def find_smallest_polymer_possible(polymer):
     reaction_sizes = []
     for letter in ascii_lowercase:
         tmp_polymer = polymer.replace(letter,'').replace(letter.upper(), '')
-        reaction_sizes.append(len(old_react_polymer(tmp_polymer)))
+        reaction_sizes.append(len(react_polymer(tmp_polymer)))
 
     return min(reaction_sizes)
 
 assert find_smallest_polymer_possible(sample) == 4
 
-print(find_smallest_polymer_possible(polymer))
+
+def main():
+    with open('input.txt', 'r') as f:
+        polymer = f.read().strip()
+
+    assert len(set(polymer)) == 52
+    assert len(set(polymer.lower())) == 26
+    part_1 = len(react_polymer(polymer))
+    part_2 = find_smallest_polymer_possible(polymer)
+    return part_1, part_2
+
+if __name__ == '__main__':
+    part_1, part_2 = main()
+    print(part_1, part_2)
