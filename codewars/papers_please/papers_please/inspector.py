6443f518e34d26c7466c8858421b94263430dffc
Ben Shaver <benpshaver@gmail.com>
2019-05-20 17:54:13 -0400

    current docs and denied countries

--- codewars/papers_please/papers_please/inspector.py
+++ codewars/papers_please/papers_please/inspector.py
@@ -1,12 +1,20 @@
 # https://www.codewars.com/kata/papers-please/train/python
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Union
+import datetime
 
 class Inspector():
-    def __init__(self) -> None:
-        self.state_dict: Dict = {
-            'allowed_countries': [],
-            'denied_countries': []
-        }
+    def __init__(self, state_dict: Union[Dict, None] = None) -> None:
+        if state_dict:
+            self.state_dict = state_dict
+        else:
+            self.state_dict: Dict = {
+                'allowed_countries': [],
+                'denied_countries': [],
+
+            }
+
+    def __repr__(self) -> str:
+        return f'Inspector({self.state_dict})'
 
     def receive_bulletin(self, bulletin: str) -> None:
         """
@@ -43,24 +51,27 @@ class Inspector():
                          for key, value  in input_entrant.items()}
 
         checks = {}
-        checks['required_docs'] = self._check_required_docs(entrant)
-        checks['no_conflicting_info'] = self._check_no_conflicting_info(entrant)
+        checks['denied_countries'] = self._check_denied_countries(entrant)
         checks['all_docs_current'] = self._check_all_docs_current(entrant)
-        checks['not_wanted_criminal'] = self._check_not_wanted_criminal(entrant)
-        checks['has_cert_of_vaccination'] = self._check_cert_of_vaccination(entrant)
-        checks['has_foreigner_paperwork'] = self._check_foreigner_paperwork(entrant)
+        try:
+            # TODO:
+            checks['required_docs'] = self._check_required_docs(entrant)
+            checks['no_conflicting_info'] = self._check_no_conflicting_info(entrant)
+            checks['all_docs_current'] = self._check_all_docs_current(entrant)
+            checks['not_wanted_criminal'] = self._check_not_wanted_criminal(entrant)
+            checks['has_cert_of_vaccination'] = self._check_cert_of_vaccination(entrant)
+            checks['has_foreigner_paperwork'] = self._check_foreigner_paperwork(entrant)
+        except:
+            pass
 
         for check, (pass_, reason) in checks.items():
             if not pass_:
-                self._fail(entrant, check=check, reason=reason)
-                break
+                return self._fail(entrant, check=check, reason=reason)
         else:
-            result = self._pass(entrant)
-
-        return result
+            return self._pass(entrant)
 
     def _pass(self, entrant: Dict) -> str:
-        if entrant['citizen']:
+        if entrant.get('citizen', False):
             return 'Glory to Arstotzka.'
         else:
             return 'Cause no trouble.'
@@ -84,24 +95,35 @@ class Inspector():
         example 2: Deny citizens of Kolechia, Republia
         """
         if instruction.startswith('Allow citizens'):
-            allowed_countries = instruction.lstrip('Allow citizens of ')
+            allowed_countries = instruction[len('Allow citizens of '):].split(',')
             self.state_dict['allowed_countries'].extend(
-                [country.strip().capitalize() for country in allowed_countries.split(',')])
+                [country.strip().capitalize() for country in allowed_countries])
+            self.state_dict['denied_countries'] = [country for country in
+                self.state_dict['denied_countries'] if country not in allowed_countries]
         elif instruction.startswith('Deny citizens'):
-            denied_countries = instruction.lstrip('Deny citizens of ')
+            denied_countries = instruction[len('Deny citizens of '):].split(',')
             self.state_dict['denied_countries'].extend(
-                [country.strip().capitalize() for country in denied_countries.split(',')])
+                [country.strip().capitalize() for country in denied_countries])
+            self.state_dict['allowed_countries'] = [country for country in
+                self.state_dict['allowed_countries'] if country not in denied_countries]
         else:
-            raise NotImplementedError(f'Can\'t hanle input: {instruction}')
+            raise NotImplementedError(f'Can\'t handle input: {instruction}')
+
 
     def _update_wanted_by_the_state(self, instruction: str) -> None:
-        raise NotImplementedError(f'Can\'t hanle input: {instruction}')
+        raise NotImplementedError(f'Can\'t handle input: {instruction}')
 
     def _update_required_vaccinations(self, instruction: str) -> None:
-        raise NotImplementedError(f'Can\'t hanle input: {instruction}')
+        raise NotImplementedError(f'Can\'t handle input: {instruction}')
 
     def _update_required_documents(self, instruction: str) -> None:
-        raise NotImplementedError(f'Can\'t hanle input: {instruction}')
+        raise NotImplementedError(f'Can\'t handle input: {instruction}')
+
+    def _check_denied_countries(self, entrant: Dict) -> Tuple[bool, str]:
+        if entrant.get('passport', {}).get('NATION') in self.state_dict['denied_countries']:
+            return False, f'Entry denied: {entrant.get("passport", {}).get("NATION", None)} in list of denied countries'
+        else:
+            return True, ''
 
     def _check_required_docs(self, entrant: Dict) -> Tuple[bool, str]:
         raise NotImplementedError(f'Unable to perform check_required_docs yet')
@@ -110,7 +132,15 @@ class Inspector():
         raise NotImplementedError(f'Unable to perform check_no_conflicting_info yet')
 
     def _check_all_docs_current(self, entrant: Dict) -> Tuple[bool, str]:
-        raise NotImplementedError(f'Unable to perform check_all_docs_current yet')
+        for doc, doc_fields in entrant.items():
+            if doc_fields.get('EXP'):
+                # Hard coded date November 22, 1982
+                # Date format 1: 1983.07.10
+                date = datetime.datetime.strptime(doc_fields['EXP'], '%Y.%m.%d')
+                if date <= datetime.datetime.strptime('1982.11.22', '%Y.%m.%d'):
+                    return False, f'Entry denied: {doc} expired'
+        else:
+            return True, ''
 
     def _check_not_wanted_criminal(self, entrant: Dict) -> Tuple[bool, str]:
         raise NotImplementedError(f'Unable to perform check_not_wanted_criminal yet')
@@ -120,3 +150,44 @@ class Inspector():
 
     def _check_foreigner_paperwork(self, entrant: Dict) -> Tuple[bool, str]:
         raise NotImplementedError(f'Unable to perform check_foreigner_paperwork yet')
+
+if __name__ == '__main__':
+
+    i = Inspector()
+    bulletin1 = """Deny citizens of Russia
+               Allow citizens of Belarus
+               """
+    i.receive_bulletin(bulletin1)
+
+    entrant1 = {
+        "passport": """ID#: GC07D-FU8AR
+        NATION: Arstotzka
+        NAME: Guyovich, Russian
+        DOB: 1933.11.28
+        SEX: M
+        ISS: East Grestin
+        EXP: 1983.07.10"""
+    }
+
+    entrant2 = {
+        "passport": """ID#: GC07D-FU8AR
+        NATION: Russia
+        NAME: Guyovich, Russian
+        DOB: 1933.11.28
+        SEX: M
+        ISS: East Grestin
+        EXP: 1983.07.10"""
+    }
+
+    entrant3 = {
+        "passport": """ID#: GC07D-FU8AR
+        NATION: Arstotzka
+        NAME: Guyovich, Russian
+        DOB: 1933.11.28
+        SEX: M
+        ISS: East Grestin
+        EXP: 1980.07.10"""
+    }
+
+    i.receive_bulletin("""Deny citizens of Belarus
+                        Allow citizens of Albania""")
--- codewars/papers_please/papers_please/tests/test_inspector.py
+++ codewars/papers_please/papers_please/tests/test_inspector.py
@@ -14,12 +14,48 @@ entrant1 = {
     EXP: 1983.07.10"""
 }
 
+def test_str_to_dict():
+    inspector = Inspector()
+    assert inspector._str_to_dict(entrant1['passport']) == {
+        'ID#': 'GC07D-FU8AR',
+        'NATION': 'Arstotzka',
+        'NAME': 'Guyovich, Russian',
+        'DOB': '1933.11.28',
+        'SEX': 'M',
+        'ISS': 'East Grestin',
+        'EXP': '1983.07.10'
+        }
+
+def test_parse_bulletin():
+    inspector = Inspector()
+    assert inspector._parse_bulletin(bulletin1) == [
+        'Deny citizens of Russia', 'Allow citizens of Belarus'
+        ]
+
 def test_denied_citizens():
     inspector = Inspector()
     inspector.receive_bulletin(bulletin1)
-    assert inspector.inspect({'passport':'NATION: Russia'}).startswith('Denied')
+    assert inspector.inspect({'passport':'NATION: Russia'}).startswith('Entry denied')
 
 def test_russia_denied_country():
     inspector = Inspector()
     inspector.receive_bulletin(bulletin1)
     assert 'Russia' in inspector.state_dict['denied_countries']
+
+def test_allowed_denied_countries_updated():
+    inspector = Inspector()
+    inspector.receive_bulletin(bulletin1)
+    assert inspector.state_dict['denied_countries'] == ['Russia']
+    assert inspector.state_dict['allowed_countries'] == ['Belarus']
+    inspector.receive_bulletin("""Deny citizens of Belarus
+                                Allow citizens of Albania""")
+    assert inspector.state_dict['denied_countries'] == ['Russia', 'Belarus']
+    assert inspector.state_dict['allowed_countries'] == ['Albania']
+
+def test_expired_passport_denied():
+    inspector = Inspector()
+    assert inspector.inspect({'passport':'EXP: 1981.12.01'}) == 'Entry denied: passport expired'
+    assert inspector.inspect({'passport':'EXP: 1982.11.22'}) == 'Entry denied: passport expired'
+
+# bulletin1 = """Deny citizens of Russia
+#                Allow citizens of Belarus
